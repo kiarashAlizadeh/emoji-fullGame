a15eeacd2d7345914c0957faab27755bae75d2ef
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-11-20 01:37:52 +0330

    some changes

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -1,30 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let signInBtn = document.getElementById('signin-btn');
+let signInBtn = document.getElementById('signin-btn');
 
-  signInBtn.addEventListener('click', function (e) {
-    e.preventDefault();
+signInBtn.addEventListener('click', function (e) {
+  e.preventDefault();
 
-    let userEmail = document.getElementById('email').value;
-    let userPass = document.getElementById('password').value;
+  let userEmail = document.getElementById('email').value;
+  let userPass = document.getElementById('password').value;
 
-    let storedData = localStorage.getItem('userData');
+  let storedData = localStorage.getItem('userData');
 
-    if (storedData) {
-      let userData = JSON.parse(storedData);
-      let storedEmail = userData[0].email;
-      let storedPassword = userData[0].password;
-        if (userEmail === '' || userPass === '') {
-            alert('Please enter your email and password')
-        }
-          else if (userEmail === storedEmail && userPass === storedPassword) {
-            localStorage.setItem('islogin', 'true');
-            alert('you are loged in');
-            window.location.href = '../Html/emoji.html';
-          } else {
-            alert('your email or your password is incorrect!');
-          }
+  if (storedData) {
+    let userData = JSON.parse(storedData);
+    let storedEmail = userData[0].email;
+    let storedPassword = userData[0].password;
+    if (userEmail === '' || userPass === '') {
+      alert('Please enter your email and password');
+    } else if (userEmail === storedEmail && userPass === storedPassword) {
+      localStorage.setItem('islogin', 'true');
+      alert('you are loged in');
+      window.location.href = '../Html/emoji.html';
     } else {
-      alert('there is no user in storage');
+      alert('your email or your password is incorrect!');
     }
-  });
+  } else {
+    alert('there is no user in storage');
+  }
 });
