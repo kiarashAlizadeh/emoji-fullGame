a733db472137dec5ee1054bf8664a737ebafaa98
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-11-23 21:54:56 +0330

    navbar bug fixed, setting bug fixed

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -1,8 +1,15 @@
-// clear local storage btn
+var isLogin = localStorage.getItem('islogin');
 let Clear = document.querySelector('.clear');
+
 Clear.addEventListener('click', () => {
+  if (isLogin === 'true') {
+    // clear local storage btn
     localStorage.removeItem('userData');
-  localStorage.removeItem('userLogin');
-  alert("Local storage has been cleared!")
-  });
-  
+    localStorage.removeItem('userLogin');
+    localStorage.setItem('islogin', 'false');
+    alert('Local storage has been cleared And you Are logOut!');
+    location.reload();
+  } else {
+    alert('Please LogIN before Clearing the local storage!');
+  }
+});
